b74d0f571e48eb7df5a831e92479a8c9a63f11df
Alaaeldin Abousamra <alaaeldinabosamra@gmail.com>
2024-06-17 07:08:50 +0300

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,28 @@
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import Dashboard from "./pages/Dashboard"
+import Athletes from "./pages/Athletes"
+import Competitions from "./pages/Competitions"
+import Blogs from "./pages/Blogs"
 function App() {
  
 
   return (
-    <>
-    <h1>hi</h1>
-    </>  
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/athletes" element={<Athletes />} />
+          <Route path="/competitions" element={<Competitions />} />
+          <Route path="/blogs" element={<Blogs />} />
+      </Routes>
+    </BrowserRouter>  
   )
 }
 
